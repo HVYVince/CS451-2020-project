f4b9962ee50ad94e63fb7b1c6899b2429533e5bd
Vincent Tournier <vincetournier@msn.com>
2020-11-12 16:43:32 +0100

    cleaned repo

--- bin/commands.sh
+++
@@ -1,6 +0,0 @@
-xterm -e "./barrier.py --host 127.0.0.1 --port 11100 --processes 4" &
-xterm -e "./finishedSignal.py --host 127.0.0.1 --port 11200 --processes 4" &
-xterm -e "./run.sh --id 1 --hosts hosts_config --barrier 127.0.0.1:11100 --signal 127.0.0.1:11200 --output output_1 config_fifo" &
-xterm -e "./run.sh --id 2 --hosts hosts_config --barrier 127.0.0.1:11100 --signal 127.0.0.1:11200 --output output_2 config_fifo" &
-xterm -e "./run.sh --id 3 --hosts hosts_config --barrier 127.0.0.1:11100 --signal 127.0.0.1:11200 --output output_3 config_fifo" &
-xterm -e "./run.sh --id 4 --hosts hosts_config --barrier 127.0.0.1:11100 --signal 127.0.0.1:11200 --output output_4 config_fifo" &
